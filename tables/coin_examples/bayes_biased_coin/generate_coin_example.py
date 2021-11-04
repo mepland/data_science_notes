6577907b6c64e16b7a880281e56a7fc501293b5d
Matthew Epland <matthew.epland@cern.ch>
2021-11-04 15:51:06 -0400

    finished coin problems

--- tables/coin_examples/bayes_biased_coin/generate_coin_example.py
+++ tables/coin_examples/bayes_biased_coin/generate_coin_example.py
@@ -1,4 +1,4 @@
-def p_unfair(h, t, n, m, p_H):
+def p_biased(h, t, n, m, p_H):
   part1 = m*(p_H**h)*((1.-p_H)**t)
   part2 = n / (2.**(h+t))
   return part1 / (part1+part2)
@@ -7,8 +7,8 @@ n = 50
 m = 50
 print('m = {m:d}, n = {n:d}'.format(m=m, n=n))
 
-with open('bayes_unfair_coin_example.tex', 'w') as f_table:
-  column_names = ['h', 't', 'p_H', 'p_unfair']
+with open('bayes_biased_coin_example.tex', 'w') as f_table:
+  column_names = ['h', 't', 'p_H', 'p_biased']
   ncol = len(column_names)
 
   f_table.write('% $m = {m:d}$, $n = {n:d}$\n\n'.format(m=m ,n=n))
@@ -28,7 +28,7 @@ with open('bayes_unfair_coin_example.tex', 'w') as f_table:
     for p_H in [1., 0.8, 0.6]:
       if t != 0 and p_H == 1.:
         p_H = 0.99 # might as well change from 1.0 when t != 0 since the P is obviously then 0.0
-      result = '{h:d}, {t:d}, {p_H:.2f}, {p_unfair:.5f}'.format(h=h, t=t, p_H=p_H, p_unfair=p_unfair(h, t, n, m, p_H))
+      result = '{h:d}, {t:d}, {p_H:.2f}, {p_biased:.5f}'.format(h=h, t=t, p_H=p_H, p_biased=p_biased(h, t, n, m, p_H))
       print(result)
       f_table.write('{0:s} \\\\\n'.format(result.replace(',', ' &')))
     f_table.write('\hline\n')
